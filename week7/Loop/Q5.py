ca83b5daf0c6f3b5930136afdb81df7c3a32a155
Nyfong <127338100+Nyfong@users.noreply.github.com>
2024-05-31 21:42:29 +0700

    Q5 loop

--- week7/Loop/Q5.py
+++ week7/Loop/Q5.py
@@ -1,12 +1,12 @@
 #cinema chair 5x5
-user_input = int(input("Enter number of Row & Col:"))
+user_input = int(input("Enter number:"))
 col_count=0
 for row in range(0,user_input):
     print("")
     print("")
     for col in range(0, user_input):
         col_count+=1
-        print(col_count, end=' ')   
+        print(col_count, end=' ')  
     
     
     
